bf09894f5d16020e4fd3ae3a42967004bddb1f25
NilsFo <nils.foerster@yahoo.de>
2025-04-07 10:08:15 +0200

    experimental main menu

--- Assets/Scripts/Beacon.cs
+++ Assets/Scripts/Beacon.cs
@@ -14,8 +14,6 @@ public class Beacon : MonoBehaviour
     public bool isInNetwork;
     public bool activeOnStart = false;
 
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
